c5b085813e248d728ebb7ec02cdf36ef403d3d77
小仙女 <me@ilyfairy.com>
2023-11-06 20:54:05 +0800

    删除发布时裁剪, 可能会导致无法运行

--- publish-linux.bat
+++ publish-linux.bat
@@ -1,4 +1,4 @@
 @echo off
-dotnet publish Ilyfairy.DstServerQuery.Web -c release -r linux-x64 --self-contained true /p:PublishSingleFile=true -p:PublishTrimmed=true -o publish\
+dotnet publish Ilyfairy.DstServerQuery.Web -c release -r linux-x64 --self-contained true /p:PublishSingleFile=true -o publish\
 echo done!
 pause>nul
